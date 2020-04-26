f2265e6b4a498b09d3bbc309d12a552fa4747024
Satoru SATOH <satoru.satoh@gmail.com>
2020-04-27 04:41:30 +0900

    fix: add some more missing test cases

--- tests/firewall.py
+++ tests/firewall.py
@@ -10,11 +10,16 @@ import fortios_xutils.parser as P
 import tests.common as C
 
 
-class TestCases_10(C.unittest.TestCase):
+class TestCases_10(C.TestCase):
 
     def test_10_df_by_query__mzero(self):
         rdf = TT.df_by_query("configs[]", {})
         self.assertTrue(rdf.empty)
+
+    def test_12_df_by_query__found(self):
+        rdf = TT.df_by_query("configs[]", dict(configs=[dict(a=1)]))
+        self.assertFalse(rdf.empty)
+        self.assertEqual(rdf.to_dict(orient="record"), [dict(a=1)])
 
     def test_30_guess_filetype(self):
         self.assertEqual(TT.guess_filetype("foo.pickle"), "pickle")
@@ -24,8 +29,6 @@ class TestCases_10(C.unittest.TestCase):
 
 
 class TestCases_20(C.TestCaseWithWorkdir):
-
-    maxDiff = None
 
     def test_10_pandas_save_load(self):
         for ext in "pickle json".split():
@@ -43,35 +46,76 @@ class TestCases_20(C.TestCaseWithWorkdir):
         self.assertRaises(ValueError, TT.pandas_load, "/a/b/c.ext_not_exist")
 
 
-class TestCases_30(C.TestCaseWithWorkdir):
+class TestCasesWithConfigs(C.TestCase):
 
-    maxDiff = None
     cpaths = C.list_res_files("show_configs", "*.txt")
 
-    def test_20_make_and_save_firewall_address_table(self):
+
+class TestCases_30(C.TestCaseWithWorkdir, TestCasesWithConfigs):
+
+    def test_20_make_and_save_firewall_policy_table(self):
         for idx, cpath in enumerate(self.cpaths):
             opath = TT.os.path.join(self.workdir, str(idx), "fw.pickle")
             cnf = P.parse_show_config(cpath)
-            rdf = TT.make_and_save_firewall_address_table(cnf, opath)
+            rdf = TT.make_and_save_firewall_policy_table(cnf, opath)
             self.assertFalse(rdf.empty)
             self.assertTrue(TT.os.path.exists(opath))
 
 
-class TestCases_50(C.unittest.TestCase):
+class TestCases_40(TestCasesWithConfigs):
 
-    maxDiff = None
+    def setUp(self):
+        self.cnfs = [P.parse_show_config(p) for p in self.cpaths]
+
+    def test_10_make_firewall_address_table_1(self):
+        for cnf in self.cnfs:
+            rdf = TT.make_firewall_address_table_1(
+                cnf,
+                has_vdoms_=P.has_vdom(cnf)
+            )
+            self.assertFalse(rdf.empty)
+            self.assertTrue("SSLVPN_TUNNEL_ADDR1" in rdf["edit"].values)
+
+    def test_20_make_firewall_addrgrp_table(self):
+        for cnf in self.cnfs:
+            rdf = TT.make_firewall_addrgrp_table(
+                cnf,
+                has_vdoms_=P.has_vdom(cnf)
+            )
+            self.assertFalse(rdf.empty)
+            self.assertTrue("G Suite" in rdf["edit"].values)
+            self.assertTrue("host_192.168.3.1" in rdf["member"].values)
+
+    def test_30_make_firewall_address_table(self):
+        for cnf in self.cnfs:
+            rdf = TT.make_firewall_address_table(
+                cnf,
+                has_vdoms_=P.has_vdom(cnf)
+            )
+            self.assertFalse(rdf.empty)
+            self.assertTrue("SSLVPN_TUNNEL_ADDR1" in rdf["edit"].values)
+            self.assertTrue("192.168.1.0/24" in rdf["addr"].values)
+            self.assertTrue("G Suite" in rdf["edit"].values)
+            self.assertTrue(any("192.168.3.3/32" in addrs
+                                for addrs in rdf["addrs"].values))
+
+
+class TestCases_50(C.unittest.TestCase):
 
     def setUp(self):
         self.cpaths = C.list_res_files("show_configs", "*.txt")
         self.cnfs = [P.parse_show_config(p) for p in self.cpaths]
         self.fdfs = [TT.make_firewall_address_table(c) for c in self.cnfs]
+        self.pdfs = [TT.make_firewall_policy_table(c) for c in self.cnfs]
 
-    def test_10_search_by_addr_1__not_found(self):
+    def test_10_search_by_addr_1__fa_not_found(self):
+        C.skip_test()
         for fdf in self.fdfs:
             rdf = TT.search_by_addr_1("127.0.0.1", fdf)
             self.assertTrue(rdf.empty)
 
-    def test_20_search_by_addr_1__found_1(self):
+    def test_20_search_by_addr_1__fa_found_1(self):
+        C.skip_test()
         for fdf in self.fdfs:
             rdf = TT.search_by_addr_1("192.168.5.201", fdf)
             self.assertFalse(rdf.empty)
