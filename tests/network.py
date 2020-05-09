81c68947d30a1b706b7933d4f9dc696b70b1e94b
Satoru SATOH <satoru.satoh@gmail.com>
2020-05-09 14:48:20 +0900

    fix: add some test cases for fortios_xutils.network

--- tests/network.py
+++ tests/network.py
@@ -5,15 +5,18 @@
 # pylint: disable=missing-docstring,invalid-name
 from __future__ import absolute_import
 
+import os.path
+
 import fortios_xutils.network as TT
 import fortios_xutils.parser as P
 import tests.common as C
 
 
-class TestCases_20(C.TestCaseWithWorkdir):
+class Base(C.TestCase):
 
     cpaths = C.list_res_files("show_configs", "*.txt")
     cnfs = [P.parse_show_config(p) for p in cpaths]
+    hnames = [P.hostname_from_configs(c, P.has_vdom(c)) for c in cnfs]
     sargss = [dict(has_vdoms_=P.has_vdom(c)) for c in cnfs]
 
     # .. seealso:: tests/res/show_configs/*.txt
@@ -26,6 +29,9 @@ class TestCases_20(C.TestCaseWithWorkdir):
                    "192.168.122.0/24",
                    "192.168.122.1/32",
                    "192.168.1.0/24"))
+
+
+class TestCases_10(Base):
 
     def test_10_list_interfaces_from_configs__no_data(self):
         for cnf, sargs in [({}, {})]:
@@ -48,5 +54,86 @@ class TestCases_20(C.TestCaseWithWorkdir):
             res = TT.networks_from_firewall_address_configs(cnf, **sargs)
             self.assertTrue(res)
             self.assertEqual(set(res), self.ref_fas)
+
+
+NG_CNF_S_1 = """
+{"configs": [{"config": "system global", "hostname": "foo-1"}]}
+"""
+
+NG_CNF_S_2 = """
+{"configs": [
+    {"config": "global",
+     "configs": [
+        {"config": "system global",
+         "hostname": "fortigate-02"}
+     ]
+    }
+  ]
+}
+"""
+
+
+def node_and_edges_from_config_file(fpath):
+    return list(TT.node_and_edges_from_config_file_itr(fpath))
+
+
+class TestCases_20(C.TestCaseWithWorkdir, Base):
+
+    def test_10_node_and_edges_from_config_file_itr__wrong_data(self):
+        cpath = os.path.join(self.workdir, "test.json")
+        open(cpath, 'w').write("{}")
+        self.assertRaises(ValueError, node_and_edges_from_config_file, cpath)
+
+        open(cpath, 'w').write(NG_CNF_S_1)
+        self.assertRaises(ValueError, node_and_edges_from_config_file, cpath)
+
+        open(cpath, 'w').write(NG_CNF_S_2)
+        self.assertRaises(ValueError, node_and_edges_from_config_file, cpath)
+
+    def test_12_node_and_edges_from_config_file_itr(self):
+        for (hname, cnf) in zip(self.hnames, self.cnfs):
+            cpath = os.path.join(self.workdir, hname, "test.json")
+            TT.utils.ensure_dir_exists(cpath)
+            TT.anyconfig.dump(cnf, cpath)
+
+            res = node_and_edges_from_config_file(cpath)
+            self.assertTrue(res)
+            self.assertTrue(any(x for x in res if x.get("type", "firewall")))
+            self.assertTrue(any(x for x in res if x.get("type", "network")))
+            self.assertTrue(any(x for x in res if x.get("type", "edge")))
+
+    def test_20_make_and_save_networks_from_config_file__no_data(self):
+        cpath = os.path.join(self.workdir, "test.json")
+        fun = TT.make_and_save_networks_from_config_file
+
+        open(cpath, 'w').write("{}")
+        self.assertRaises(ValueError, fun, cpath)
+
+        open(cpath, 'w').write(NG_CNF_S_1)
+        self.assertRaises(ValueError, fun, cpath)
+
+        open(cpath, 'w').write(NG_CNF_S_2)
+        self.assertRaises(ValueError, fun, cpath)
+
+    def test_22_make_and_save_networks_from_config_file(self):
+        for (hname, cnf) in zip(self.hnames, self.cnfs):
+            cpath = os.path.join(self.workdir, hname, "test.json")
+            TT.utils.ensure_dir_exists(cpath)
+            TT.anyconfig.dump(cnf, cpath)
+
+            res = TT.make_and_save_networks_from_config_file(cpath)
+            self.assertTrue(res)
+
+    def test_30_make_and_save_networks_from_config_files(self):
+        cpaths = [os.path.join(self.workdir, hname, "test.json")
+                  for hname in self.hnames]
+
+        for idx, cnf in enumerate(self.cnfs):
+            TT.utils.ensure_dir_exists(cpaths[idx])
+            TT.anyconfig.dump(cnf, cpaths[idx])
+
+        fun = TT.make_and_save_networks_from_config_files_itr
+        for fpath, res in fun(cpaths):
+            self.assertTrue(res)
 
 # vim:sw=4:ts=4:et:
