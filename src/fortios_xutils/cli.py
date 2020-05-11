884be1df7be16f012e27067de4f7aac1f8328903
Satoru SATOH <satoru.satoh@gmail.com>
2020-05-12 05:21:13 +0900

    fix: [cli] correct and add more help text for each commands

--- src/fortios_xutils/cli.py
+++ src/fortios_xutils/cli.py
@@ -43,8 +43,47 @@ def expand_glob_paths_itr(filepaths):
                     "input filepath]"), default=None)
 def parse(filepaths, outdir):
     """
+    Parse fortigate CLI's "show *configuration* outputs and generate a
+    structured JSON file. FILEPATHS is a list of file paths or a glob pattern
+    gives that.
+
+    Examples:
+
+    \b
+        $ fortios_xutils parse -O /tmp/0 \\
+        > tests/res/show_configs/fortigate_cli_show_sample_*.txt
+        $ ls /tmp/0
+        fortigate-01  fortigate-02
+        $ ls /tmp/0/fortigate-01:
+        all.json                         firewall_address.json
+        firewall_addrgrp.json            firewall_policy.json
+        firewall_service_category.json   firewall_service_custom.json
+        firewall_service_group.json      metadata.json
+        system_global.json               system_interface.json
+        system_object-tagging.json       system_replacemsg-group.json
+        system_settings.json
+        $ jq '.' /tmp/0/fortigate-01/system_interface.json
+        [
+          {
+            "edit": "dmz",
+            "vdom": "root",
+            "status": "down",
+            "type": "physical",
+            "role": "dmz",
+            "snmp-index": "1"
+          },
+          {
+            "edit": "port1",
+            "vdom": "root",
+            "ip": [
+              "192.168.122.10",
+              "255.255.255.0"
+            ],
+                ... (snip) ...
+    \f
     :param filepaths:
         A list of path of the input fortios' "show *configuration" output
+
     :param outdir: Dir to save parsed results as JSON files
     """
     fsit = expand_glob_paths_itr(filepaths)
@@ -68,9 +107,31 @@ def parse_json_files_itr(filepaths, path_exp):
 @click.command()
 @click.argument("filepaths", nargs=-1,
                 type=click.Path(exists=True, readable=True))
-@click.option("-P", "--path", "path_exp")
+@click.option("-P", "--path", "path_exp", help="JMESPath expression to query")
 def search(filepaths, path_exp):
     """
+    Search an item or items from JSON file generated previously by 'parse' sub
+    command. FILEPATHS is a list of file paths or a glob pattern gives that.
+
+    Examples:
+
+    \b
+        $ # List ip addresses of system interfaces.
+        $ fortios_xutils search \\
+        > -P "configs[?config=='system interface'].edits[].ip" \\
+        > tests/res/parsed/fortigate-01/all.json
+        [
+            [
+                "192.168.122.10",
+                "255.255.255.0"
+            ],
+            [
+                "192.168.1.10",
+                "255.255.255.0"
+            ]
+        ]
+        $
+    \f
     :param filepaths:
         A list of the JSON input file paths. Each JSON file contains parsed
         results
@@ -91,11 +152,31 @@ def search(filepaths, path_exp):
 @click.option("-P", "--prefix", help="Max network prefix [24]", default=24)
 def network_collect(filepaths, prefix):
     """
-    Make and save network data collected from the fortigate's configurations.
+    Make and save network data collected from the JSON structured fortigate's
+    configuration files. FILEPATHS is a list of path of the JSON file, the
+    parsed results of fortigate CLI's "show \\*configuration" outputs.
 
+    Examples:
+
+    \b
+        $ fortios_xutils network-collect tests/res/parsed/*/all.json
+        $ head -n 10 tests/res/parsed/fortigate-01/networks.yml
+        metadata:
+          type: metadata
+          input: tests/res/parsed/fortigate-01/all.json
+          prefix: 24
+          timestamp: 2020-05-12_04_58_57
+          version: '1.0'
+        nodes:
+        - id: fortigate-01
+          name: fortigate-01
+          type: firewall
+        $
+
+    \f
     :param filepaths:
         A list of path of the input JSON file which is the parsed results of
-        fortios' "show *configuration" outpath
+        fortios' "show *configuration" outputs
     :param prefix: Max network prefix to search networks for
     """
     fpaths = list(expand_glob_paths_itr(filepaths))
@@ -111,9 +192,30 @@ def network_collect(filepaths, prefix):
               default=None)
 def network_compose(filepaths, outpath):
     """
-    Compose network data collected from the fortigate's configurations from
-    multiple fortigate hosts.
+    Compose network files collected from the fortigate CLI's configurations
+    from multiple fortigate hosts using the preivous network-collect command,
+    into a network file.
 
+    Examples:
+
+    \b
+        $ fortios_xutils network-compose \\
+        > tests/res/parsed/fortigate-0*/networks.yml \\
+        > -o tests/res/networks/all.yml
+        $ head -n 10 tests/res/networks/all.yml
+        metadata:
+          inputs:
+          - tests/res/parsed/fortigate-01/all.json
+          - tests/res/parsed/fortigate-02/all.json
+          timestamp: 2020-05-12_05_02_49
+          version: '1.0'
+        nodes:
+        - id: fortigate-01
+          name: fortigate-01
+          type: firewall
+        $
+
+    \f
     :param filepaths:
         A list of network graph data files generated by `network_collect` sub
         command in advance.
@@ -132,6 +234,17 @@ def firewall_policy_save(filepath, outpath):
     """
     Make and save firewall policy table (:class:`pandas.DataFrame` object).
 
+    Examples:
+
+    \b
+        $ fortios_xutils firewall-policy-save \\
+        > -o /tmp/0/test.pickle.gz \\
+        > tests/res/parsed/fortigate-01/all.json
+        $ file /tmp/0/test.pickle.gz
+        /tmp/0/test.pickle.gz: gzip compressed data, was "test.pickle"  ...
+        $
+
+    \f
     :param filepath:
         Path of the input JSON file which is the parsed results of fortios'
         "show *configuration" outpath
@@ -152,8 +265,43 @@ def firewall_policy_save(filepath, outpath):
               help="Specify an IP address to search")
 def firewall_policy_search(filepath, ip_s):
     """
-    Search firewall policy by IP address
+    Search firewall policy table generated by 'firewall-policy-save' command,
+    by ip address. FILEPATH is a file path to the pandas dataframe file
+    generated by 'firewall-policy-save' command.
 
+    Examples:
+
+    \b
+        $ fortios_xutils firewall-policy-search \\
+        > --ip 192.168.122.3 /tmp/0/test.pickle.gz
+        [
+          {
+            "edit": "20",
+            "name": "Monitor_Servers_02",
+            "uuid": "3da73baa-dacb-48cb-852c-c4be245b4609",
+            "srcintf": "port1",
+            "dstintf": "",
+            "srcaddr": "host_192.168.122.1",
+            "dstaddr": "network_192.168.122.0/24",
+            "action": "accept",
+            "schedule": "always",
+            "service": [
+              "HTTPS",
+              "HTTP"
+            ],
+            "inspection-mode": "",
+            "nat": "",
+            "srcaddrs": [
+              "192.168.122.1/32"
+            ],
+            "dstaddrs": [
+              "192.168.122.0/24"
+            ],
+            "comments": ""
+          }
+        ]
+
+    \f
     :param filepath:
         Path of the json file contains parsed results of fortios' "show
         *configuration" outputs, or pandas.DataFrame data file, or the
@@ -183,6 +331,53 @@ def network_find_paths(filepath, src_ip, dst_ip, ntype=None):
     """
     Search paths from the source `src_ip` to the destination `dst_ip`.
 
+    Examples:
+
+    \b
+        $ fortios_xutils/cli.py network-find-paths \\
+        > tests/res/networks/graph.yml 192.168.122.2 192.168.5.10
+        [
+          [
+            {
+              "id": "fortigate-01",
+              "name": "fortigate-01",
+              "type": "firewall",
+              "addrs": [
+                "192.168.122.10/24",
+                "192.168.1.10/24"
+              ],
+              "type_id": 5,
+              "class": "node firewall",
+              "label": "fortigate-01 firewall"
+            },
+            {
+              "id": "192.168.122.0/24",
+              "name": "192.168.122.0/24",
+              "type": "network",
+              "addrs": [
+                "192.168.122.0/24"
+              ],
+              "type_id": 1,
+              "class": "node network",
+              "label": "192.168.122.0/24 network"
+            },
+                ... (snip) ...
+            {
+              "id": "192.168.5.0/24",
+              "name": "192.168.5.0/24",
+              "type": "network",
+              "addrs": [
+                "192.168.5.0/24"
+              ],
+              "type_id": 1,
+              "class": "node network",
+              "label": "192.168.5.0/24 network"
+            }
+          ],
+            ... (snip) ...
+        $
+
+    \f
     :param filepath:
         Path of the json or yaml file contains netowrk nodes and links
         information.
