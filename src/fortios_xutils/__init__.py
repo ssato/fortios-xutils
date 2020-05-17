3069f12dbf2e7877fa1efbd1269a129a9a7fe9a6
Satoru SATOH <satoru.satoh@gmail.com>
2020-05-17 16:05:05 +0900

    change: import api.* (API functions) to export

--- src/fortios_xutils/__init__.py
+++ src/fortios_xutils/__init__.py
@@ -1,5 +1,34 @@
 r"""Very experimental miscellaneous and extra utilities for fortios.
 """
 from __future__ import absolute_import
+from .api import (  # noqa: F401
+    parse_and_save_show_configs,
+    query_json_files,
+    collect_networks,
+    collect_and_save_networks,
+    compose_networks,
+    compose_and_save_networks,
+    make_firewall_policy_table,
+    make_firewall_policy_tables,
+    make_and_save_firewall_policy_tables,
+    load_firewall_policy_table,
+    search_firewall_policy_table_by_addr
+)
+
 
 __version__ = "0.2.0"
+__all__ = """
+parse_and_save_show_configs
+query_json_files
+collect_networks
+collect_and_save_networks
+compose_networks
+compose_and_save_networks
+make_firewall_policy_table
+make_firewall_policy_tables
+make_and_save_firewall_policy_tables
+load_firewall_policy_table
+search_firewall_policy_table_by_addr
+""".split()
+
+# vim:sw=4:ts=4:et:
