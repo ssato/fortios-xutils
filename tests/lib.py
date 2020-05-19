7b348d5b7727410404c48645749e47f22ccc371b
Satoru SATOH <satoru.satoh@gmail.com>
2020-05-20 02:51:28 +0900

    enhancement: add test cases for . (lib itself)

---
+++ tests/lib.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2020 Satoru SATOH <satoru.satoh@gmail.com>
+# SPDX-License-Identifier: MIT
+#
+# pylint: disable=missing-docstring,invalid-name
+from __future__ import absolute_import
+
+import fortios_xutils as TT
+import tests.api as API
+
+
+class TestCase(API.TestCase):
+
+    mod = TT
+
+# vim:sw=4:ts=4:et:
